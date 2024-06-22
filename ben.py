3ff2f6e6ec4e205b7a1638e49705651e48265f93
Sanwar09 <sanwarbendsure09@gmail.com>
2024-06-22 17:09:07 +0530

    Oops

--- ben.py
+++ ben.py
@@ -1,1 +1,9 @@
-print("Hello")
+class A:
+    def Show_data(self):
+        print("I am in class A")
+class B:
+    def Show_data(self):
+        print("I am in class B")
+obj=B()
+obj.Show_data()
+print("The Opps conecept")
